8ce10bebb979704ad24f5891077368b3e591170e
Mahfijur Rahman <161872772+Mahfuj85@users.noreply.github.com>
2024-05-25 11:24:30 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let calBody = document.querySelector('.calculator-body');
+let btn = document.querySelectorAll('button>h5');
+let screen = document.getElementById('screen');
+
+for(let i=0; i<btn.length; i++){
+    btn[i].addEventListener("click", () => {
+        if(btn[i].innerHTML == "="){
+            screen.innerHTML = eval(screen.innerHTML);
+        } else{
+            if(btn[i].innerHTML =="C"){
+                screen.innerHTML = "";
+            } else{
+                screen.innerHTML += btn[i].innerHTML;
+            }
+        }
+    })
+}
+
